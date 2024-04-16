7efbe54de7e41036ed4a34b6d242666b8c4d6422
mcchri <112865223+mcchri@users.noreply.github.com>
2024-04-16 21:36:21 +0100

    Update revision3.py

--- revision3.py
+++ revision3.py
@@ -30,33 +30,56 @@ def new_password():
         for i in score_list:
             if i == 1:
                 score+=1
-        return score,new_paswrd        
-        
-print("1) Create a new User ID \n2) Change a password \n3) Display all the User Ids \n4) Quit")
-print("")
-selct = int(input("Enter Selection: "))
-user_IDs = []
-correct = False
-if selct == 1:
-    
-    ID_lis = new_user_ID(user_IDs)
-    user_IDs = ID_lis[0]
-    user_ID = ID_lis[1]
-    while correct == False:
-        lis_pass = new_password()
-        score = lis_pass[0]
-        if score < 3:
-            print("It is a weak password")
-        elif score < 5:
-            print("This password could be improved")
-            choice = input("Do you want to make another password: (yes/no) ")
-            if choice == "no":
-                correct = True
-        elif score == 5:
-            print("You have a strong password")
-            correct = True
+        return score,new_paswrd
+
+def GUI():
+    ok = 0
+    while ok == 0:        
+        print("1) Create a new User ID \n2) Change a password \n3) Display all the User Ids \n4) Quit")
+        print("")
+        selct = int(input("Enter Selection: "))
+        user_IDs = []
+        correct = False
+
+        if selct == 1:
+            ID_lis = new_user_ID(user_IDs)
+            user_IDs = ID_lis[0]
+            user_ID = ID_lis[1]
+            while correct == False:
+                lis_pass = new_password()
+                score = lis_pass[0]
+                if score < 3:
+                    print("It is a weak password")
+                elif score < 5:
+                    print("This password could be improved")
+                    choice = input("Do you want to make another password: (yes/no) ")
+                    if choice == "no":
+                        correct = True
+                elif score == 5:
+                    print("You have a strong password")
+                    correct = True
+            f = open("IDs.csv",'w')
+            user_ID = user_ID + ","
+            string_com = user_ID + lis_pass[1]
+            f.write(string_com)
+            f.close()
             
-f = open("IDs.csv",'w')
-string_com = 
-f.write(user_ID,lis_pass)
-f.close()
+        elif selct == 2:
+            change_id = input("Enter the existing ID that you want to change the password: ")
+            f = open("IDs.csv",'r')
+            for line in f:
+                x=line.split(",")
+                if change_id == x[0]:
+                    lis_pass = new_password()
+                    f = ''.replace(x[1],lis_pass[1])
+            f = open("IDs.csv",'r')
+            f.close()
+            
+        elif selct == 3:
+            f = open("IDs.csv",'r')
+            for line in f:
+                x=line.split(",")
+                print(x[0])
+        else:
+            ok+=1
+GUI()
